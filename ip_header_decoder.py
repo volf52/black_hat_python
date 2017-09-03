d4d4054945bc2af22a4e6d36af541531696c0605
Muhammad Arslan <rslnkrmt2552@gmail.com>
2017-09-04 00:50:12 +0500

    Host Discovery

--- ip_header_decoder.py
+++ ip_header_decoder.py
@@ -6,12 +6,29 @@
 """
 
 import os
+import threading
+import time
+from netaddr import IPNetwork, IPAddress
 import socket
 import struct
 from ctypes import *
 from get_ip import get_lan_ip
 
 host = get_lan_ip()
+subnet = '.'.join(host.split('.')[:2]) + '.86.0/24'
+
+magic_message = "VOLF"
+
+def udp_sender(host, subnet, magic_message):
+    time.sleep(5)
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    addr = filter(lambda x: x != host, IPNetwork(subnet))
+    for ip in addr:
+        print "Trying %s" % ip
+        try:
+            sender.sendto(magic_message, ("%s" % ip, 65212))
+        except:
+            pass
 
 class IP(Structure):
     _fields_ = [
@@ -44,6 +61,22 @@ class IP(Structure):
             self.protocol = str(self.protocol_num)
 
 
+class ICMP(Structure):
+    _fields_ = [
+        ("type",        c_ubyte),
+        ("code",        c_ubyte),
+        ("checksum",    c_ushort),
+        ("unused",      c_ushort),
+        ("next_hop_mtu",c_ushort)
+    ]
+
+    def __new__(cls, socket_buffer):
+        return cls.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        pass
+
+
 if os.name == "nt":
     socket_protocol = socket.IPPROTO_IP
 else:
@@ -57,11 +90,29 @@ sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 if os.name == "nt":
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
+
+t = threading.Thread(target=udp_sender, args=(host, subnet, magic_message))
+t.start()
+
+
 try:
     while True:
         raw_buffer = sniffer.recvfrom(65565)[0]
         ip_header = IP(raw_buffer[:20])
         print "Protocol: %s\t%s -> %s" % (ip_header.protocol, ip_header.src_addr, ip_header.dst_addr)
+
+        if ip_header.protocol == "ICMP":
+
+            offset = ip_header.ihl * 4
+            buf = raw_buffer[offset: offset + sizeof(ICMP)]
+            icmp_header = ICMP(buf)
+            print "ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code)
+
+            if icmp_header.code == 3 and icmp_header.type == 3:
+                if IPAddress(ip_header.src_addr) in IPNetwork(subnet):
+                    if raw_buffer[len(raw_buffer) - len(magic_message):] == magic_message:
+                        print "Host Up: %s" % ip_header.src_addr
+
 
 except KeyboardInterrupt:
     if os.name == "nt":
