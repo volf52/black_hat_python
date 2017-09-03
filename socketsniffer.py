ee3e67718e6fa2e33cfec243c0f3333af13a08f9
Muhammad Arslan <rslnkrmt2552@gmail.com>
2017-09-03 19:58:34 +0500

    Socket Sniffer start

---
+++ socketsniffer.py
@@ -0,0 +1,27 @@
+import socket
+import os
+
+#host to listen on
+host = raw_input("Enter your IP > ")
+
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host, 0))
+
+#allow ip headers in captured packets
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+# in windows, need o set IOCTL to set up promiscous mode
+
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+print sniffer.recvfrom(65565)
+
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
